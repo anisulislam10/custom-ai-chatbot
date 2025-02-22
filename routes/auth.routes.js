6fe9e0162899daa37821ab05a2aada8f4dc43bdb
Anisul Islam <anis.inbox10@gmail.com>
2025-02-23 02:33:34 +0500

    added user meta data get

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,16 +1,16 @@
 import express from "express";
 import { registerUser, loginUser, verifyUser, forgotPassword, resetPassword,getUser } from "../controllers/auth.controller.js";
-import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { protect  } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
 // Routes
 router.post("/register", registerUser);
+router.get("/profile", protect, getUser);
 router.post("/login", loginUser);
 router.get("/verify/:token", verifyUser); 
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
-router.get("/profile", authMiddleware, getUser);
 
 
 export default router;
